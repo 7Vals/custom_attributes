09ba1787b30896889ce718e3a7f9591e93de009a
Tariq Hussain <tariq.hussain@7vals.com>
2015-06-29 13:52:01 +0500

    Allow edit and delete links to be passed completely to the partial.

--- lib/generators/custom_attributes/templates/migration.rb
+++ lib/generators/custom_attributes/templates/migration.rb
@@ -19,7 +19,7 @@ class CreateCustomAttributesFor<%= name %> < ActiveRecord::Migration
       t.string    :type
       t.text      :string_value
       t.integer   :integer_value
-      t.decimal   :double_value
+      t.decimal   :double_value, precision: 54, scale: 9
       t.datetime  :date_time_value
       t.references :<%= singular_table_name %>, foreign_key: true
       t.references :<%= singular_table_name %>_custom_attribute_definition, foreign_key: true
