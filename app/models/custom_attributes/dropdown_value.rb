57fb31458aeb14da6089f90414bb04fbb1138669
hafsabatool7 <hafsa.batool@7vals.com>
2020-03-20 12:07:03 +0500

    Inline editing changes zen theme refs #14022

--- app/models/custom_attributes/dropdown_value.rb
+++ app/models/custom_attributes/dropdown_value.rb
@@ -4,6 +4,7 @@ module CustomAttributes
       custom_attribute_option = custom_attribute_options.find_by id: custom_attribute_option_id
       if (custom_attribute_option_value = custom_attribute_option_values.first).present?
         if custom_attribute_option.present?
+          
           custom_attribute_option_value.custom_attribute_option = custom_attribute_option
           custom_attribute_option_value.save
         else
