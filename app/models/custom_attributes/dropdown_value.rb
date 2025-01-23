c425301e917cd5ddec8f4e15f9a9f29e966e221c
Abdul Haseeb <abdul.haseeb@7vals.com>
2025-01-23 19:35:55 +0500

    store value for linkable resource

--- app/models/custom_attributes/dropdown_value.rb
+++ app/models/custom_attributes/dropdown_value.rb
@@ -1,6 +1,9 @@
 module CustomAttributes
   module DropdownValue
-    def value=(custom_attribute_option_id)
+    def value=(args)
+      _set_values_for_linkable_resource(args) and return if args.is_a?(Hash)
+
+      custom_attribute_option_id = args
       custom_attribute_option = custom_attribute_options.find_by id: custom_attribute_option_id
       if (custom_attribute_option_value = custom_attribute_option_values.first).present?
         if custom_attribute_option.present?
@@ -26,6 +29,14 @@ module CustomAttributes
 
     def selected_option_id
       custom_attribute_option_values.first.try(:custom_attribute_option).try(:id)
+    end
+
+    private
+
+    # args = { linkable_resource_id:, linkable_resource_type:}
+    def _set_values_for_linkable_resource(args)
+      self.linkable_resource_id = args[:linkable_resource_id]
+      self.linkable_resource_type = args[:linkable_resource_type]
     end
   end
 end
