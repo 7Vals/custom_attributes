dd12533633e524752d3c9ba56e83de38b7c2a413
Abdul Haseeb <abdul.haseeb@7vals.com>
2025-01-27 15:11:26 +0500

    Definition of linkable resource methods refs: RM#37226

--- app/models/custom_attributes/custom_attribute_option.rb
+++ app/models/custom_attributes/custom_attribute_option.rb
@@ -5,6 +5,15 @@ module CustomAttributes
       validates :label, presence: { message: 'Option cannot be blank, either remove the blank option or add a value to it' }
       validates :label, format: { with: /\A[a-zA-Z\_\s0-9]+\z/, message: 'cannot contain special characters.' }, if: -> { linkable_resource_id.blank? }
       default_scope -> { order(:position) }
+      belongs_to :linkable_resource, polymorphic: true
+    end
+
+    def option
+      linkable_resource_id.blank? ? read_attribute(:option) : linkable_resource_display_value
+    end
+
+    def linkable_resource_display_value
+      CustomAttributeDefinitionLinkedModule.linkable_resource_display_value(linkable_resource)
     end
   end
 end
