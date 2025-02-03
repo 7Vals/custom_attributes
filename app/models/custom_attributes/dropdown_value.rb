708c6f5950ed747b3c99e939e283425be3f28128
Abdul Haseeb <abdul.haseeb@7vals.com>
2025-02-03 15:03:54 +0500

    Comments resolving refs: RM#37226

--- app/models/custom_attributes/dropdown_value.rb
+++ app/models/custom_attributes/dropdown_value.rb
@@ -30,18 +30,9 @@ module CustomAttributes
 
     def value
       if respond_to?(:linkable_resource_id) && linkable_resource_id.present?
-        linkable_resource_display_value || DEFAULT_FIELD_VALUE
+        linkable_resource_display_value
       else
         custom_attribute_option_values.first.try(:custom_attribute_option).try(:label)
-      end
-    end
-
-    def field_value(c_attr = custom_attribute_defn)
-      if c_attr.linked_to_module_via_items_filtered_by_criteria?
-        f_value = linkable_resource_display_value
-        f_value == DEFAULT_FIELD_VALUE ? '' : f_value
-      else
-        value
       end
     end
 
