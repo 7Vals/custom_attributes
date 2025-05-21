89dad0ded2adca7b527ef2e826a23207ad31a47c
aliahmad7 <ali.ahmad@7vals.com>
2025-05-21 19:19:48 +0500

    changes related to big decimal method refs# 41042

--- lib/generators/custom_attributes/templates/application_helper.rb
+++ lib/generators/custom_attributes/templates/application_helper.rb
@@ -12,19 +12,5 @@ module CustomAttributes
         CustomAttributes::CustomAttribute::TYPE_DROPDOWN        => "Dropdown"
       }
     end
-
-    def format_numeric_custom_attr_val(value)
-      return value unless value.is_a?(BigDecimal)
-
-      # Here are examples
-      # 13.5 → 13.50
-      # 13 → 13.00
-      # 13.12 → 13.12
-      # 13.123445 → 13.123445
-      # 13.000001 → 13.000001
-      # 13.0000 → 13.00
-      num = BigDecimal(value.to_s)
-      num.frac.zero? ? format('%.2f', num) : num.to_s('F')
-    end
   end
 end
